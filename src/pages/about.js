be7148cdcf4f63c247de89c2925b1e3fceec551e
dylan belouis <dylan.belouis@gmail.com>
2019-11-04 11:27:59 +0100

    mise en place de la structure du site

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="about Gatsby"/>
+    <Header headerText="it's pretty cool" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
